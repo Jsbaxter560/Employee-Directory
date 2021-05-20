f4f96c4100bc9cbc62db98691e58c613e0126588
John Baxter <juancho420@ymail.com>
2021-05-19 17:52:21 -0700

    created react app and added components folder and pages folder

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
